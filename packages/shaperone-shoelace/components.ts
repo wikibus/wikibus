dfa78767bd9a4f099f442a11c1d41480d60f20fa
tpluscode <tpluscode@users.noreply.github.com>
2022-06-12 15:32:52 +0200

    feat: textbox with lang

--- packages/shaperone-shoelace/components.ts
+++ packages/shaperone-shoelace/components.ts
@@ -1,6 +1,10 @@
 import { Lazy, SingleEditorComponent } from '@hydrofoil/shaperone-wc'
 import { dash } from '@tpluscode/rdf-ns-builders/strict'
 import type { ComponentInstance } from '@hydrofoil/shaperone-core/models/components'
+import { html } from 'lit'
+import rdf from '@rdfjs/data-model'
+import { isGraphPointer, isLiteral } from 'is-graph-pointer'
+import type { GraphPointer } from 'clownface'
 
 interface EditorState extends ComponentInstance {
   noLabel?: boolean
@@ -12,6 +16,50 @@ export const textField: Lazy<SingleEditorComponent<EditorState>> = {
     const { inputRenderer } = await import('./renderer/input')
 
     return inputRenderer()
+  },
+}
+
+interface TextFieldWithLang extends ComponentInstance {
+  language?: string
+}
+
+export const textFieldWithLang: Lazy<SingleEditorComponent<TextFieldWithLang>> = {
+  editor: dash.TextFieldWithLangEditor,
+  async lazyRender() {
+    const [{ inputRenderer }] = await Promise.all([
+      import('./renderer/input'),
+      import('./components/sh-sl-with-lang-editor'),
+    ])
+
+    function extractLanguage(ptr: GraphPointer | undefined) {
+      return isLiteral(ptr) ? ptr.term.language : ''
+    }
+
+    return function (object, actions) {
+      function valueChanged(value: string) {
+        actions.update(rdf.literal(value, object.value.componentState.language))
+      }
+
+      function languageChanged(e: any) {
+        let language: string | undefined
+        if (e.detail.value) {
+          language = e.detail.value
+        }
+
+        if (isGraphPointer(object.value.object)) {
+          actions.update(rdf.literal(object.value.object.value, language))
+        } else {
+          object.updateComponentState({ language })
+        }
+      }
+
+      return html`
+        <sh-sl-with-lang-editor .languages="${object.property.shape.languageIn}"
+                                .language="${extractLanguage(object.value.object)}"
+                                @language-selected="${languageChanged}">
+          ${inputRenderer({ onChange: valueChanged })(object, actions)}
+        </sh-sl-with-lang-editor>`
+    }
   },
 }
 
--- packages/shaperone-shoelace/renderer/input.ts
+++ packages/shaperone-shoelace/renderer/input.ts
@@ -1,14 +1,18 @@
 import type { SlInput } from '@shoelace-style/shoelace'
 import { html, SingleEditorComponent } from '@hydrofoil/shaperone-wc'
+import '@shoelace-style/shoelace/dist/components/input/input.js'
 
 interface InputRenderer {
   type?: SlInput['type']
+  onChange?(value: string): void
 }
 
-export const inputRenderer = async (arg: InputRenderer = {}): Promise<SingleEditorComponent['render']> => {
-  await import('@shoelace-style/shoelace/dist/components/input/input.js')
+export function inputRenderer(arg: InputRenderer = {}): SingleEditorComponent['render'] {
+  return ({ value }, { update }) => {
+    const onChange = arg.onChange || update
 
-  return ({ value }, { update }) => html`<sl-input .value="${value.object?.value || ''}"                                                    
-                                                   .type="${arg.type}"
-                                                   @sl-change="${(e: any) => update(e.target.value)}"></sl-input>`
+    return html`<sl-input .value="${value.object?.value || ''}"
+                          .type="${arg.type}"
+                          @sl-change="${(e: any) => onChange(e.target.value)}"></sl-input>`
+  }
 }
