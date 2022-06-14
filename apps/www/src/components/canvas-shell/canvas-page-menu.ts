7bd06810994a066bc5e9effe2ae49225a94a1d1a
tpluscode <tpluscode@users.noreply.github.com>
2022-06-14 22:49:03 +0200

    responsive subpage menu

--- apps/www/src/components/canvas-shell/canvas-page-menu.ts
+++ apps/www/src/components/canvas-shell/canvas-page-menu.ts
@@ -1,12 +1,44 @@
-import { html, LitElement } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { css, html, LitElement } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
 import CanvasShellBase from './CanvasShellBase'
 
 @customElement('canvas-page-menu')
 export class CanvasPageMenu extends CanvasShellBase(LitElement) {
+  static get styles() {
+    return [
+      super.styles || [],
+      css`
+        :host([open]) slot::slotted(nav) {
+          display: block !important;
+        }
+      `,
+    ]
+  }
+
+  @property({ type: Boolean, reflect: true })
+    open?: boolean
+
+  __hideOnOutsideClick: any
+
+  constructor() {
+    super()
+    this.__hideOnOutsideClick = (e: Event) => {
+      if (!e.composedPath().includes(this)) {
+        this.open = false
+      }
+    }
+  }
+
   connectedCallback() {
     super.connectedCallback?.()
     this.id = 'page-menu'
+
+    document.addEventListener('click', this.__hideOnOutsideClick)
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback?.()
+    document.removeEventListener('click', this.__hideOnOutsideClick)
   }
 
   render() {
@@ -16,9 +48,16 @@ export class CanvasPageMenu extends CanvasShellBase(LitElement) {
           <div class="container">
             <div class="page-menu-row">
               <slot></slot>
+              <div id="page-submenu-trigger" @click="${this.__toggle}" @keyup="${this.__toggle}">
+                <i class="icon-reorder"></i>
+              </div>
             </div>
           </div>
         </div>
       </div>`
+  }
+
+  __toggle() {
+    this.open = !this.open
   }
 }
