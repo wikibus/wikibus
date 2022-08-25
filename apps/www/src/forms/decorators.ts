8d32942d4ee1c9584b4ce9636fdf5542a21f7c37
tpluscode <tpluscode@users.noreply.github.com>
2022-08-25 22:06:37 +0200

    refactor: simplify decorator

--- apps/www/src/forms/decorators.ts
+++ apps/www/src/forms/decorators.ts
@@ -1,25 +1,20 @@
 import { html } from '@hydrofoil/shaperone-wc'
+import { decorate } from '@hydrofoil/shaperone-wc/renderer/decorator.js'
 import { ObjectTemplate } from '@hydrofoil/shaperone-wc/templates'
 import TermSet from '@rdfjs/term-set'
 import { sh1 } from '../lib/ns'
 
-export function objectFilter(inner: ObjectTemplate): ObjectTemplate {
-  const decorated: ObjectTemplate = (context, arg) => {
-    const excludedPointer = context.property.shape.pointer.out(sh1.except)
-    if (excludedPointer.isList()) {
-      const excluded = new TermSet([...excludedPointer.list()].map(({ term }) => term))
+export const objectFilter = decorate((inner: ObjectTemplate) => (context, arg) => {
+  const excludedPointer = context.property.shape.pointer.out(sh1.except)
+  if (excludedPointer.isList()) {
+    const excluded = new TermSet([...excludedPointer.list()].map(({ term }) => term))
 
-      const isExcluded = arg.object.object && excluded.has(arg.object.object.term)
+    const isExcluded = arg.object.object && excluded.has(arg.object.object.term)
 
-      if (isExcluded) {
-        return html``
-      }
+    if (isExcluded) {
+      return html``
     }
-
-    return inner(context, arg)
   }
 
-  decorated.loadDependencies = inner.loadDependencies
-
-  return decorated
-}
+  return inner(context, arg)
+})
